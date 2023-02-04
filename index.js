38bc13cf97779a351bbac5ad728d33a5493f6234
anthony <anthonytguilherme@hotmail.com>
2023-02-04 19:06:47 -0300

    delete some trash code

--- index.js
+++ index.js
@@ -8,29 +8,27 @@ const passwordError = document.querySelector('.password-error');
 
 
 
-function ValidateEmail () {
+function ValidateEmail() {
   const validRegexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-  if (!email.value.match(validRegexEmail)){
+  if (!email.value.match(validRegexEmail)) {
     return emailError.innerHTML = 'Invalid Email';
-  } emailError.innerHTML = '';
+  }
+  emailError.innerHTML = '';
+
 }
-function ValidatePassword () {
+function ValidatePassword() {
   const validRegexPassword = /^[A-Za-z0-9]\w{8,}$/;
 
-  if(!password.value.match(validRegexPassword)){
+  if (!password.value.match(validRegexPassword)) {
     return passwordError.innerHTML = 'Invalid Passowrd'
-  } passwordError.innerHTML = '';
+  }
+  passwordError.innerHTML = '';
+
 }
-
-
 
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  
- ValidateEmail();
- ValidatePassword();
 
- window.location.href="./checked.html";
 })
